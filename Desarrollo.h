7b09e3793b9fea8f5aeb9a495a9a32d2c2ff4778
Patricio Altamiranda <68668425+dejameponerunnombre@users.noreply.github.com>
2025-10-24 20:02:44 -0300

    Update Desarrollo.h

--- Desarrollo.h
+++ Desarrollo.h
@@ -27,10 +27,29 @@ typedef struct{
     char region[10];
 }Taperturas;
 
+typedef struct  // partecica del punto 6
+{
+    char Fecha[11];           
+    char Descripcion[31];     
+    char Periodo[7];          
+    double Indice_IPC;        
+    char Region[10];          
+    char Grupo[11];     // Bienes: Alimentos y bebidas no alcohólicas, Bebidas alcohólicas y tabaco, Prendas de vestir y calzado, Bienes y servicios varios, Equipamiento y mantenimiento del hogar. 
+} Tanalisis_IPC;        //Servicios: Recreación y cultura, Restaurantes y hoteles, Salud, Transporte, Educación, Comunicación, Vivienda, agua, electricidad, gas y otros combustibles.
+
+
 //declaro mis funciones
 
 void decodificarFecha(char *periodo);
 void reescribirFecha(char *fechaNum);
 int mi_atoi(char *cadena);
 
+void leoTxtDivisiones(FILE *pt, Tdivisiones *divi);
+void nomralizardesc(char *v); //3
+void comaApunto(char *v); //4y8
+double calcularMontoAjustado(Tdivisiones *divi);//5
+void leoTxtAperturas(FILE *pt, Taperturas *ap);
+void mi_strncpy(char *dest, const char *src, int n) //por las dudas
+
 #endif // DESARROLLO_H_INCLUDED
+
