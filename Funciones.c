d694dc1d012349ec9291662e48b59e1fdcd76db9
Patricio Altamiranda <68668425+dejameponerunnombre@users.noreply.github.com>
2025-10-24 20:04:52 -0300

    Update Funciones.c

--- Funciones.c
+++ Funciones.c
@@ -74,3 +74,199 @@ void reescribirFecha(char *fechaNum)
     }
     *(fechaNum+i) = '\0';
 }
+
+void leoTxtDivisiones(FILE *pt, Tdivisiones *divi) //lactura primer archivo
+{
+    char linea[LONGFILE];
+    fgets(linea, LONGFILE, pt);
+    char *act = strchr(linea, '\n');
+    if(!act)
+        act = strchr(linea, '\0');
+    *act = '\0';
+
+    act -= 6;
+    strncpy(divi->Periodo_codificado, act, 6);
+    divi->Periodo_codificado[6] = '\0';
+    *act = '\0';
+
+    act -= 9;
+    strncpy(divi->region, act, 9);
+    divi->region[9] = '\0';
+    *act = '\0';
+
+    act -= 16;
+    sscanf(act, "%lf", &divi->v_i_a_IPC);
+    *act = '\0';
+
+    act -= 16;
+    sscanf(act, "%lf", &divi->v_m_ipc);
+    *act = '\0';
+    
+    act -= 16;
+    /*
+    strncpy(aux, act, 16);      PARA EL PUNTO 4 YO LO HARIA ASI
+    aux[16] = '\0';
+    comaApunto(aux);
+    */
+    sscanf(act, "%lf", &divi->Indice_ipc);
+    *act = '\0';
+
+    act -= 30;
+    strncpy(divi->Clasificador, act, 30);
+    divi->Clasificador[30] = '\0';
+    *act = '\0';
+
+    act -= 30;
+    strncpy(divi->Descripcion, act, 30);
+    divi->Descripcion[30] = '\0';
+    *act = '\0';
+
+    act -= 20;
+    strncpy(divi->Codigo, act, 20);
+    divi->Codigo[20] = '\0';
+}
+
+void nomralizardesc(char * v) //punto 3
+{
+    char *aux=v;
+    int pri=0; //control primera letra
+    while(*aux!='\0')
+    {
+        if(*aux>='A' && *aux<='Z' && pri!=0)
+            *aux=*aux-'A'+'a';
+        else if(pri==0 && *aux >= 'a' && *aux <= 'z')
+            *aux=*aux-'a'+'A';
+        aux++;
+        pri=1;
+    }
+}
+
+void comaApunto(char *v) //punto 4 y 8
+{
+    char *aux = v;
+    while(*aux != '\0')
+    {
+        if(*aux == ',')
+            *aux = '.';
+        aux++;
+    }
+}
+
+void mi_strncpy(char *dest, const char *src, int n)
+{
+    int i = 0;
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+}
+
+
+double calcularMontoAjustado(Tdivisiones *divi) //punto 5
+{
+    double monto;
+    int opcion;
+    char region[15];
+    char desde[7], hasta[7];
+
+    printf("Ingrese el monto: $ ");
+    scanf("%lf", &monto);
+    
+    do
+    {
+        printf("\nSeleccione la region:\n 1. Nacional\n 2. GBA\n 3. Pampeana\n 4. Cuyo\n 5. Noroeste\n 6. Noreste\n 7. Patagonia\n");
+        scanf("%d", &opcion);
+    }while(opcion<1||opcion>7)
+
+    switch (opcion) 
+    {
+        case 1:
+            strcpy(region, "Nacional");
+            break;
+        case 2:
+            strcpy(region, "GBA");
+            break;
+        case 3:
+            strcpy(region, "Pampeana");
+            break;
+        case 4:
+            strcpy(region, "Cuyo");
+            break;
+        case 5: 
+            strcpy(region, "Noroeste");
+            break;
+        case 6:
+            strcpy(region, "Noreste");
+            break;
+        case 7: 
+            strcpy(region, "Patagonia");
+            break;
+    }
+
+    printf("\nIngrese el periodo desde (formato AAAAMM): ");
+    scanf("%6s", desde);
+    printf("Ingrese el periodo hasta (formato AAAAMM): ");
+    scanf("%6s", hasta);
+    
+    //mi cara cuando leo el resto del punto 5 6 y 9 (⊙_⊙)
+
+
+}
+
+void leoTxtAperturas(FILE *pt, Taperturas *ap) //lectura segundo archivo
+{
+    char linea[LONGFILE];
+    fgets(linea, LONGFILE, pt);
+    char *act = strchr(linea, '\n');
+    if(!act)
+        act = strchr(linea, '\0');
+    *act = '\0';
+
+    act -= 9;
+    strncpy(ap->region, act, 9);
+    ap->region[9] = '\0';
+    *act = '\0';
+
+    act -= 16;
+    sscanf(act, "%lf", &ap->v_i_a_IPC);
+    *act = '\0';
+
+    act -= 16;
+    sscanf(act, "%lf", &ap->v_m_ipc);
+    *act = '\0';
+
+    act -= 16;
+    /*
+    strncpy(aux, act, 16);      PARA EL PUNTO 8 YO LO HARIA ASI
+    aux[16] = '\0';
+    comaApunto(aux);
+    */
+    sscanf(act, "%lf", &ap->Indice_ipc);
+    *act = '\0';
+
+    act -= 6;
+    strncpy(ap->Periodo, act, 6);
+    ap->Periodo[6] = '\0';
+    *act = '\0';
+
+    act -= 30;
+    strncpy(ap->Clasificador, act, 30);
+    ap->Clasificador[30] = '\0';
+    *act = '\0';
+
+    act -= 30;
+    strncpy(ap->Descripcion_aperturas, act, 30);
+    ap->Descripcion_aperturas[30] = '\0';
+    *act = '\0';
+
+    act -= 20;
+    strncpy(ap->Codigo, act, 20);
+    ap->Codigo[20] = '\0';
+}
